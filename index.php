d7df4fec3b1023e7cfa2b5defdda487539ab1739
Sam Hurwitz <contact.samhurwitz@gmail.com>
2023-06-10 23:55:56 +0500

    lesson19: upload

--- index.php
+++ index.php
@@ -2,14 +2,84 @@
 function buildGallery($dir)
 {
     $files = scandir($dir);
-    $files = array_diff($files, array('.', '..', '.DS_Store'));
     echo '<div class="gallery">';
     foreach ($files as $file) {
         if (preg_match('/\.(jpg|jpeg|png|gif)$/i', $file)) {
-            echo '<img src="' . $dir . '/' . $file . '" onclick="window.open(\'' . $dir . '/' . $file . '\', \'_blank\')">';
+            echo '<img src="' . $dir . '/thumbnails/' . $file . '" onclick="window.open(\'' . $dir . '/' . $file . '\', \'_blank\')">';
         }
     }
     echo '</div>';
+}
+
+function resizeImage($source, $destination, $maxWidth, $maxHeight)
+{
+    list($width, $height, $type) = getimagesize($source);
+    $ratio = min($maxWidth / $width, $maxHeight / $height);
+
+    $newWidth = round($ratio * $width);
+    $newHeight = round($ratio * $height);
+
+    switch ($type) {
+        case IMAGETYPE_JPEG:
+            $sourceImage = imagecreatefromjpeg($source);
+            break;
+        case IMAGETYPE_PNG:
+            $sourceImage = imagecreatefrompng($source);
+            break;
+        case IMAGETYPE_GIF:
+            $sourceImage = imagecreatefromgif($source);
+            break;
+        default:
+            return false;
+    }
+
+    $destinationImage = imagecreatetruecolor($newWidth, $newHeight);
+    imagecopyresampled($destinationImage, $sourceImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+    switch ($type) {
+        case IMAGETYPE_JPEG:
+            imagejpeg($destinationImage, $destination);
+            break;
+        case IMAGETYPE_PNG:
+            imagepng($destinationImage, $destination);
+            break;
+        case IMAGETYPE_GIF:
+            imagegif($destinationImage, $destination);
+            break;
+        default:
+            return false;
+    }
+
+    imagedestroy($sourceImage);
+    imagedestroy($destinationImage);
+
+    return true;
+}
+
+if (isset($_FILES['image'])) {
+    $allowedTypes = array(IMAGETYPE_JPEG, IMAGETYPE_PNG, IMAGETYPE_GIF);
+    $detectedType = exif_imagetype($_FILES['image']['tmp_name']);
+    if (!in_array($detectedType, $allowedTypes)) {
+        echo 'Unsupported filetype';
+        exit;
+    }
+
+    $maxSize = 1024 * 1024;
+    if ($_FILES['image']['size'] > $maxSize) {
+        echo 'File too large';
+        exit;
+    }
+
+    $filename = uniqid() . '.jpg';
+
+    $originalPath = 'img/' . $filename;
+    move_uploaded_file($_FILES['image']['tmp_name'], $originalPath);
+
+    $thumbnailPath = 'img/thumbnails/' . $filename;
+    resizeImage($originalPath, $thumbnailPath, 200, 200);
+
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit;
 }
 ?>
 <!DOCTYPE html>
@@ -30,11 +100,29 @@ function buildGallery($dir)
             width: 200px;
             cursor: pointer;
         }
+
+        form {
+            margin-top: 20px;
+        }
+
+        input[type="file"] {
+            display: block;
+            margin-bottom: 10px;
+        }
+
+        input[type="submit"] {
+            display: block;
+            margin-top: 10px;
+        }
     </style>
 </head>
 
 <body>
     <?php buildGallery('img'); ?>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="image">
+        <input type="submit" value="Загрузить">
+    </form>
 </body>
 
 </html>
