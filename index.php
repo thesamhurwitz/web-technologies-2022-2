04d8a17f291f7c39277275f60e8e88ceeab40121
Sam Hurwitz <contact.samhurwitz@gmail.com>
2023-06-10 23:42:22 +0500

    lesson19: task 1

--- index.php
+++ index.php
@@ -1,58 +1,39 @@
 <?php
-$title = 'Current Time';
-
-function getCurrentTime()
-{
-    $hour = date('H');
-    $minute = date('i');
-
-    $hourText = getNumEnding($hour, array('час', 'часа', 'часов'));
-    $minuteText = getNumEnding($minute, array('минута', 'минуты', 'минут'));
-
-    return "$hourText $minuteText";
-}
-
-function getNumEnding($number, $endings)
-{
-    $number = $number % 100;
-    if ($number >= 11 && $number <= 19) {
-        $ending = $endings[2];
-    } else {
-        $i = $number % 10;
-        switch ($i) {
-            case (1):
-                $ending = $endings[0];
-                break;
-            case (2):
-            case (3):
-            case (4):
-                $ending = $endings[1];
-                break;
-            default:
-                $ending = $endings[2];
-        }
-    }
-    return "$number $ending";
-}
+$photos = array(
+    "img/photo1.jpg",
+    "img/photo2.jpg",
+    "img/photo3.png",
+    "img/photo4.jpg",
+    "img/photo5.jpg"
+);
 ?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="UTF-8">
-    <title>
-        <?php echo $title; ?>
-    </title>
+    <title>Gallery</title>
+    <style>
+        .gallery {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+        }
+
+        .gallery img {
+            margin: 10px;
+            width: 200px;
+            cursor: pointer;
+        }
+    </style>
 </head>
 
 <body>
-    <main>
-        <p>Текущее время:
-            <?php echo getCurrentTime(); ?>
-        </p>
-    </main>
-
+    <div class="gallery">
+        <?php foreach ($photos as $photo): ?>
+        <img src="<?php echo $photo; ?>" onclick="window.open('<?php echo $photo; ?>', '_blank')">
+        <?php endforeach; ?>
+    </div>
 </body>
 
 </html>
