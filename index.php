e27b677b028707d857ffc71a3f7a85e4cacef3fe
Sam Hurwitz <contact.samhurwitz@gmail.com>
2023-06-10 23:37:00 +0500

    lesson18

--- index.php
+++ index.php
@@ -1,58 +1,155 @@
-<?php
-$title = 'Current Time';
-
-function getCurrentTime()
-{
-    $hour = date('H');
-    $minute = date('i');
-
-    $hourText = getNumEnding($hour, array('час', 'часа', 'часов'));
-    $minuteText = getNumEnding($minute, array('минута', 'минуты', 'минут'));
-
-    return "$hourText $minuteText";
-}
-
-function getNumEnding($number, $endings)
-{
-    $number = $number % 100;
-    if ($number >= 11 && $number <= 19) {
-        $ending = $endings[2];
-    } else {
-        $i = $number % 10;
-        switch ($i) {
-            case (1):
-                $ending = $endings[0];
-                break;
-            case (2):
-            case (3):
-            case (4):
-                $ending = $endings[1];
-                break;
-            default:
-                $ending = $endings[2];
-        }
-    }
-    return "$number $ending";
-}
-?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="UTF-8">
-    <title>
-        <?php echo $title; ?>
-    </title>
 </head>
 
 <body>
-    <main>
-        <p>Текущее время:
-            <?php echo getCurrentTime(); ?>
-        </p>
-    </main>
+    <section>
+        <h2>Task 1</h2>
+        <?php
+        $i = 0;
+        do {
+            if ($i == 0) {
+                echo "$i - это ноль.<br>";
+            } elseif ($i % 2 == 0) {
+                echo "$i - чётное число.<br>";
+            } else {
+                echo "$i - нечётное число.<br>";
+            }
+            $i++;
+        } while ($i <= 10);
+        ?>
+    </section>
 
+    <section>
+        <h2>Task 2</h2>
+        <?php
+        $regions = [
+            "Московская область" => ["Москва", "Зеленоград", "Клин"],
+            "Ленинградская область" => ["Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"],
+            "Рязанская область" => ["Рязань", "Касимов", "Скопин"]
+        ];
+
+        foreach ($regions as $region => $cities) {
+            echo "$region:<br>";
+            foreach ($cities as $city) {
+                echo "$city, ";
+            }
+            echo "<br>";
+        }
+        ?>
+    </section>
+
+    <section>
+        <h2>Task 3</h2>
+        <?php
+        $translit = [
+            'а' => 'a',
+            'б' => 'b',
+            'в' => 'v',
+            'г' => 'g',
+            'д' => 'd',
+            'е' => 'e',
+            'ё' => 'yo',
+            'ж' => 'zh',
+            'з' => 'z',
+            'и' => 'i',
+            'й' => 'y',
+            'к' => 'k',
+            'л' => 'l',
+            'м' => 'm',
+            'н' => 'n',
+            'о' => 'o',
+            'п' => 'p',
+            'р' => 'r',
+            'с' => 's',
+            'т' => 't',
+            'у' => 'u',
+            'ф' => 'f',
+            'х' => 'h',
+            'ц' => 'ts',
+            'ч' => 'ch',
+            'ш' => 'sh',
+            'щ' => 'sch',
+            'ъ' => '',
+            'ы' => 'y',
+            'ь' => '',
+            'э' => 'e',
+            'ю' => 'yu',
+            'я' => 'ya'
+        ];
+
+        function translit($str)
+        {
+            global $translit;
+            $result = '';
+            for ($i = 0; $i < mb_strlen($str); $i++) {
+                $char = mb_substr($str, $i, 1);
+
+                if (isset($translit[$char])) {
+                    $result .= $translit[$char];
+                } else {
+                    $result .= $char;
+                }
+            }
+            return $result;
+        }
+
+        echo translit("задание номер три");
+        ?>
+    </section>
+
+    <section>
+        <h2>Task 4</h2>
+        <?php
+        $menu = [
+            "Home",
+            "About",
+            "Services" => ["Web", "Mobile", "SEO"],
+            "Contacts"
+        ];
+
+        function generateMenu($menu)
+        {
+            echo "<ul>";
+            foreach ($menu as $key => $item) {
+                if (is_array($item)) {
+                    echo "<li>$key";
+                    generateMenu($item);
+                    echo "</li>";
+                } else {
+                    echo "<li>$item</li>";
+                }
+            }
+            echo "</ul>";
+        }
+
+        generateMenu($menu);
+        ?>
+    </section>
+
+    <section>
+        <h2>Task 6</h2>
+        <?php
+        $regions = [
+            "Московская область" => ["Москва", "Зеленоград", "Клин"],
+            "Ленинградская область" => ["Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"],
+            "Рязанская область" => ["Рязань", "Касимов", "Скопин"]
+        ];
+
+        foreach ($regions as $region => $cities) {
+            echo "$region:<br>";
+            foreach ($cities as $city) {
+                if (mb_substr($city, 0, 1) == "К") {
+                    echo "$city, ";
+                }
+            }
+            echo "<br>";
+        }
+        ?>
+    </section>
 </body>
 
 </html>
